0ef9f85d6f187015c7a24bd9ad02847ac7345568
Yauheni1234 <p.zhenia_1990@mail.ru>
2024-03-21 17:46:57 +0300

    add date

--- FirstClass/Program.cs
+++ FirstClass/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello Евгений");
+Console.WriteLine("Hello Евгений 21.03.24");
 string user = Console.ReadLine();
 Console.WriteLine(user);
