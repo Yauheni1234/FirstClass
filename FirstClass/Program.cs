e6a1e9fd8a5cedf2d63c644afb67e93359b06749
Yauheni1234 <p.zhenia_1990@mail.ru>
2024-03-21 17:24:33 +0300

    add surname

--- FirstClass/Program.cs
+++ FirstClass/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello Евгений");
+Console.WriteLine("Hello Евгений Пашкович");
 string user = Console.ReadLine();
 Console.WriteLine(user);
-123456
