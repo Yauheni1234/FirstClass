5af520e5114974d41c8ffc9087b58e70e11a1cff
Yauheni1234 <p.zhenia_1990@mail.ru>
2024-04-02 17:16:06 +0300

    Homework to lesson 1

--- FirstClass/Program.cs
+++ FirstClass/Program.cs
@@ -4,3 +4,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello Евгений 21.03.24");
 string user = Console.ReadLine();
 Console.WriteLine(user);
+
+string word = "Welcome";
+string word2 = "to";
+string word3 = "the";
+string word4 = "TMS";
+string word5 = "lesons";
+string text = word + " " + word2 + " " + word3 " " + word4 + " " _word5;
+Console.WriteLine(text);
